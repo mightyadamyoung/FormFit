519c2e7c8cd043f119458b5d9daf5446307db51b
sbowde4 <sbowde4@lsu.edu>
2017-03-09 12:52:11 -0600

    removed password

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$db_connect = mysqli_connect("localhost", "formfitdata", "Du4ld1m3ns10ns", "formfit_users");
+$db_connect = mysqli_connect("localhost", "yourdatabase", "yourpasss", "yourdbname");
 //check if connection error
 if (mysqli_connect_errno()){
 	echo mysqli_connect_error();
