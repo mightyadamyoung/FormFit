de7c0fe021a14aef737f98479349a060a3dd7f96
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-01 22:26:44 -0500

    Update newuser.php

--- newuser.php
+++ newuser.php
@@ -1,3 +1,16 @@
+/****************************************************************************************/
+/*
+/* FILE NAME: newuser.php
+/*
+/* DESCRIPTION: Code for handling registration of a new user.
+/*
+/* REFERENCE:
+/*
+/*   DATE      BY       DESCRIPTION
+/* ========   ======   =============
+/* 4/1/2017 Sam Bowden  Created file before header initializations
+/*
+/****************************************************************************************/
 <?php
 session_start();
 // If user is logged in, header them away
