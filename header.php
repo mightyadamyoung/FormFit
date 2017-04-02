c1a78e02801ffe1ec3e8cf48e51d8dbe2e86106a
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-01 22:22:56 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,16 @@
+/****************************************************************************************/
+/*
+/* FILE NAME: header.php
+/*
+/* DESCRIPTION: 
+/*
+/* REFERENCE:
+/*
+/*   DATE      BY       DESCRIPTION
+/* ========   ======   =============
+/* 4/1/2017 Sam Bowden  Created file before header initializations
+/*
+/****************************************************************************************/
 <!DOCTYPE html>
 <html>
 <meta charset="UTF-8">
