2615e846a3cf3959b100c03ea072bfad3d64c654
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-01 22:23:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+/****************************************************************************************/
+/*
+/* FILE NAME: index.php
+/*
+/* DESCRIPTION: 
+/*
+/* REFERENCE:
+/*
+/*   DATE      BY       DESCRIPTION
+/* ========   ======   =============
+/* 4/1/2017 Sam Bowden  Created file before header initializations
+/*
+/****************************************************************************************/
 <?php
 include("includes/check_login_status.php");
 // If user is already logged in, header that weenis away
