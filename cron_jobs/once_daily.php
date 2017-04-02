80bb6dd3f5e6a4c34b2e00bf165e14a5f41d17ec
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-01 22:30:32 -0500

    Update once_daily.php

--- cron_jobs/once_daily.php
+++ cron_jobs/once_daily.php
@@ -1,3 +1,16 @@
+/****************************************************************************************/
+/*
+/* FILE NAME: once_daily.php
+/*
+/* DESCRIPTION: 
+/*
+/* REFERENCE:
+/*
+/*   DATE      BY       DESCRIPTION
+/* ========   ======   =============
+/* 4/1/2017 Sam Bowden  Created file before header initializations
+/*
+/****************************************************************************************/
 <?php 
 require_once("../includes/db_connect.php");
 //delete all accounts not activated within 3 days
