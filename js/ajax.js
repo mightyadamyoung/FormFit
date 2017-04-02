8ad457dde16a9ed7da6e599c4ea8fdc92dbe9d81
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-01 22:36:13 -0500

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,3 +1,16 @@
+/****************************************************************************************/
+/*
+/* FILE NAME: ajax.js
+/*
+/* DESCRIPTION: 
+/*
+/* REFERENCE:
+/*
+/*   DATE      BY       DESCRIPTION
+/* ========   ======   =============
+/* 4/1/2017 Sam Bowden  Created file before header initializations
+/*
+/****************************************************************************************/
 function ajaxObj( meth, url ) {
 	var x = new XMLHttpRequest();
 	x.open( meth, url, true );
