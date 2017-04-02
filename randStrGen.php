46da28ed2df17a5cd8987b5994c19b1d5ea000ee
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-01 22:28:12 -0500

    Update randStrGen.php

--- randStrGen.php
+++ randStrGen.php
@@ -1,3 +1,16 @@
+/****************************************************************************************/
+/*
+/* FILE NAME: randStrGen.php
+/*
+/* DESCRIPTION: 
+/*
+/* REFERENCE:
+/*
+/*   DATE      BY       DESCRIPTION
+/* ========   ======   =============
+/* 4/1/2017 Sam Bowden  Created file before header initializations
+/*
+/****************************************************************************************/
 <?php
 function randStrGen($len){
 	$result = "";
