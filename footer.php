aadb35f8cb8314ae22d6a945f222b2df18965102
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-01 22:21:32 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+/****************************************************************************************/
+/*
+/* FILE NAME: footer.php
+/*
+/* DESCRIPTION: 
+/*
+/* REFERENCE:
+/*
+/*   DATE      BY       DESCRIPTION
+/* ========   ======   =============
+/* 4/1/2017 Sam Bowden  Created file before header initializations
+/*
+/****************************************************************************************/
 </div>
 <div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px"><p class="w3-center">&copy FormFit 2017</div>
 </html>
