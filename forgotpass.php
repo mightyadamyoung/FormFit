91f26e419597a2301e3ca507bdce33f54e221890
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-23 12:56:11 -0500

    Update forgotpass.php

--- forgotpass.php
+++ forgotpass.php
@@ -4,8 +4,6 @@
 /*
 /* DESCRIPTION: Code for reseting a users forgotten/lost password.
 /*
-/* REFERENCE:
-/*
 /*   DATE      BY       DESCRIPTION
 /* ========   ======   =============
 /* 4/1/2017 Sam Bowden  Created file before header initializations
