52f426e7b070bf2a89c37487d663cfebb7659550
Mitchell Aucoin <mightyadamyoung@users.noreply.github.com>
2017-04-23 14:48:08 -0500

    Update index.php

--- users/index.php
+++ users/index.php
@@ -2,9 +2,7 @@
 /*
 /* FILE NAME: index.php
 /*
-/* DESCRIPTION: 
-/*
-/* REFERENCE:
+/* DESCRIPTION: Code for handling style and format of the page relevent to user's view.
 /*
 /*   DATE      BY       DESCRIPTION
 /* ========   ======   =============
